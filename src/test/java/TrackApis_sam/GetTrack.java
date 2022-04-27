ca0f55f5f566f61c4287c0e13b4206635ad73830
shampriya <shampriya@deloitte.com>
2022-04-27 21:36:01 +0530

    Track_Allocated

---
+++ src/test/java/TrackApis_sam/GetTrack.java
@@ -0,0 +1,33 @@
+package TrackApis_sam;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+public class GetTrack {
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+
+    @BeforeClass
+    public void setup() {
+        String BaseURI = "https://huallocation-backend-urtjok3rza-wl.a.run.app/HUAllocation";
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(BaseURI).addHeader("Content-Type", "application/json");
+        requestSpecification = RestAssured.with().spec(requestSpecBuilder.build());
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().expectContentType(ContentType.JSON);
+        responseSpecification = responseSpecBuilder.build();
+    }
+
+    @Test(priority = 1)
+    public void Get_Track_Allocated() {
+        requestSpecification.given()
+                .when().
+                get("/track-allocated").
+                then().spec(responseSpecification).statusCode(200).log().status()
+                .extract().response();
+    }
+
+}
