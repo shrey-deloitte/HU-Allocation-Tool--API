60773929e21263c7673a0d893303e5cd5b4b16e9
shampriya <shampriya@deloitte.com>
2022-04-28 15:09:15 +0530

    Track_Allocation_And_Analysis

--- src/test/java/TrackApis_sam/GetTrack.java
+++ src/test/java/TrackApis_sam/GetTrack.java
@@ -3,11 +3,30 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class GetTrack {
+    public static Properties prop;
+    static {
+        try{
+            prop = new Properties();
+            FileInputStream ip=new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\resources\\getdata.properties");
+            prop.load(ip);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     RequestSpecification requestSpecification;
     ResponseSpecification responseSpecification;
 
@@ -23,54 +42,85 @@ public class GetTrack {
 
     @Test(priority = 1)
     public void Get_Track_Allocated() {
-        requestSpecification.given()
+       Response response = requestSpecification.given()
                 .when().
                 get("/track-allocated").
                 then().spec(responseSpecification).statusCode(200).log().status()
                 .extract().response();
+        JSONArray arr = new JSONArray(response.asString());
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj = arr.getJSONObject(i);
+            assert obj.get("assigned_track") instanceof String;
+        }
+
     }
     @Test(priority = 2)
     public void Get_Track_Email() {
-        requestSpecification.given()
+       Response response =  requestSpecification.given()
                 .when().
-                get("/track-allocated/email/jtrembath1i").
+                get("/track-allocated/email/" + prop.getProperty("email")).
                 then().spec(responseSpecification).statusCode(200).log().status()
                 .extract().response();
+        System.out.println(response.getStatusCode());
+        ResponseBody body = response.getBody();
+        System.out.println(body.asString());
+
+        JSONArray arr = new JSONArray(response.asString());
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj = arr.getJSONObject(i);
+            assert obj.get("email") instanceof String;
+        }
 
     }
     @Test(priority = 3)
     public void Get_Perform_Track_YES() {
-        requestSpecification.given()
+       Response response= requestSpecification.given()
                 .when().
                 get("/perform-track-analysis-rate-yes").
                 then().spec(responseSpecification).statusCode(200).log().status()
                 .extract().response();
+        JSONObject obj = new JSONObject(response.asString());
+        assert obj.get("message") instanceof String;
+        Assert.assertEquals(obj.get("message"),"Done");
 
     }
     @Test(priority = 4)
     public void Get_Perform_Track_NO() {
-        requestSpecification.given()
+        Response response = requestSpecification.given()
                 .when().
                 get("/perform-track-analysis-only-rate-no").
                 then().spec(responseSpecification).statusCode(200).log().status()
                 .extract().response();
-
+        JSONObject obj = new JSONObject(response.asString());
+        assert obj.get("message") instanceof String;
+        Assert.assertEquals(obj.get("message"),"Done");
     }
     @Test(priority = 5)
     public void Get_Perform_Track_Score() {
-        requestSpecification.given()
+      Response response=   requestSpecification.given()
                 .when().
                 get("/perform-track-analysis-score-no").
                 then().spec(responseSpecification).statusCode(200).log().status()
                 .extract().response();
+        JSONObject obj = new JSONObject(response.asString());
+        assert obj.get("message") instanceof String;
+        Assert.assertEquals(obj.get("message"),"Done");
+
     }
     @Test(priority = 6)
     public void Get_Parallel_Track() {
-        requestSpecification.given()
+        Response response = requestSpecification.given()
                 .when().
-                get("/parallel-track-pref").
-                then().spec(responseSpecification).statusCode(200).log().status()
-                .extract().response();
+                get("/parallel-track-pref/email/" + prop.getProperty("email")).
+                then().spec(responseSpecification).statusCode(200).extract().response();
+        System.out.println(response.getStatusCode());
+        ResponseBody body = response.getBody();
+        System.out.println(body.asString());
+
+        JSONObject obj = new JSONObject(response.asString());
+        assert obj.get("email") instanceof String;
+        Assert.assertEquals(obj.get("email"),prop.getProperty("email"));
+
 
     }
 }
